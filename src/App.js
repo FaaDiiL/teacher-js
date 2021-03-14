e51233d17ff15ab1b9e81009d350e7ff6379ada2
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-14 23:11:38 +0100

    Added a reducer to handleSubmit

--- src/App.js
+++ src/App.js
@@ -13,17 +13,36 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const carSpect = [{model:'BMW',regNr:'ABC123'}]
+const carSpect2 = [{model:'Toyota',regNr:'ATY323'}]
 function App() {
-
+  
   function registerCar(model,regNr,port=carSpect){
+    // port.push({model,regNr})
+    
     port.push({model,regNr})
+
   }
   function handleSubmit(e) {
     e.preventDefault()
-    let model = e.target[0].value
-    let regNr = e.target[1].value
+    const elementArr = [...e.currentTarget.querySelectorAll('*[name]')];
+    // console.log('elementArr: ',elementArr)
+
+
+    const formData = elementArr.reduce((prev, next) => {
+      const inputName = next.getAttribute('name');//model
+      const inputValue = next.value;//'dsadsa'
+    
+      prev[inputName] = inputValue;
+      console.log('Prev: ',prev)
+      return prev;
+    }, {});
+    // console.log(formData.model)
+    let model = formData.model
+    let regNr = formData.regNr
+    // registerCar(model,regNr)
     registerCar(model,regNr)
     console.log(carSpect)
+
   }
   const classes = useStyles()
 
@@ -31,6 +50,7 @@ function App() {
     let obj = carSpect.shift()
     console.log(`${obj.model} with registration number ${obj.regNr} come to port 1`)
     console.log(carSpect)
+    console.log(carSpect2)
   }
   return (
     <div>
@@ -40,8 +60,8 @@ function App() {
         <Grid container spacing={3}>
           <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
             <Grid xs={12} sm={12} md={6} item>
-            <TextField id="standard-basic" label="AUDI, BMW ..." />
-            <TextField id="standard-basic" label="ABC123 ..." />
+            <TextField id="standard-basic" name='model' label="AUDI, BMW ..." />
+            <TextField id="standard-basic" name='regNr' label="ABC123 ..." />
             <Button onClick={callCar} variant="contained" color="primary" fullWidth>
                 Call Car
             </Button>
