e31542dc148818fb4460754ef14f7c1d58237d18
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-14 12:34:31 +0100

    adding car, remove car, adding remove btn.

--- src/App.js
+++ src/App.js
@@ -12,24 +12,42 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-
+const carSpect = [{model:'BMW',regNr:'ABC123'}]
 function App() {
 
-  
+  function registerCar(model,regNr,port=carSpect){
+    port.push({model,regNr})
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+    let model = e.target[0].value
+    let regNr = e.target[1].value
+    registerCar(model,regNr)
+    console.log(carSpect)
+  }
   const classes = useStyles()
+
+  function callCar(){
+    let obj = carSpect.shift()
+    console.log(`${obj.model} with registration number ${obj.regNr} come to port 1`)
+    console.log(carSpect)
+  }
   return (
     <div>
       <CssBaseline />
       <Container maxWidth="md">
         <h1>CarSpect</h1>
         <Grid container spacing={3}>
-          <form className={classes.root} noValidate autoComplete="off">
+          <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
             <Grid xs={12} sm={12} md={6} item>
             <TextField id="standard-basic" label="AUDI, BMW ..." />
             <TextField id="standard-basic" label="ABC123 ..." />
-              <Button variant="contained" color="primary" fullWidth>
+            <Button onClick={callCar} variant="contained" color="primary" fullWidth>
+                Call Car
+            </Button>
+            <Button type={'submit'}variant="contained" color="primary" fullWidth>
                 Add
-              </Button>
+            </Button>
             </Grid>
           </form>
           </Grid>
