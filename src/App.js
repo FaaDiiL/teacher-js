5e284db170793331744a252432d41a13a89172a7
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-15 00:43:50 +0100

    Restructure the app...

--- src/App.js
+++ src/App.js
@@ -14,44 +14,68 @@ const useStyles = makeStyles((theme) => ({
 
 const carSpect = [{model:'BMW',regNr:'ABC123'}]
 const carSpect2 = [{model:'Toyota',regNr:'ATY323'}]
-function App() {
+
+
+
+// TODO 1.0 
+  // Create a function that takes two parameters: "model, regNr"
+  // Add the value from the parameters to the end of the array: carSpect by a es6 HOF
   
-  function registerCar(model,regNr,port=carSpect){
-    // port.push({model,regNr})
-    
-    port.push({model,regNr})
-
+  // ANCHOR - This is the Add Function
+  function addCar (model, regNr){
+    carSpect.push({model, regNr})
   }
-  function handleSubmit(e) {
-    e.preventDefault()
-    const elementArr = [...e.currentTarget.querySelectorAll('*[name]')];
-    // console.log('elementArr: ',elementArr)
-
-
-    const formData = elementArr.reduce((prev, next) => {
-      const inputName = next.getAttribute('name');//model
-      const inputValue = next.value;//'dsadsa'
+  // addCar('honda', 'her345')
+  // console.log(carSpect)
+  
+  // ANCHOR - This is the CallFunction
+    // Create a function called callCar with no parameters
+    // Remove the first item from the array and use its return values to write a msg like
+    // NOTE 'model', 'regNr' drive to port 1.
     
-      prev[inputName] = inputValue;
-      console.log('Prev: ',prev)
-      return prev;
-    }, {});
-    // console.log(formData.model)
-    let model = formData.model
-    let regNr = formData.regNr
-    // registerCar(model,regNr)
-    registerCar(model,regNr)
-    console.log(carSpect)
+    function callCar (){
+      let callCarMsg 
+      if(carSpect.length > 0 ){
+      let removedCar = carSpect.shift()
+      callCarMsg= `${removedCar.model} ${removedCar.regNr} Come to port one!`
+      } else {
+        callCarMsg = 'No cars to call!'
+      }
+      console.log(callCarMsg)
+    }
+  
 
-  }
+    // TODO Register Car on Submit
+      // Create a function handleSubmit with e as parameter
+      // Ling the function with the form.
+      // add type submit on the btn submit
+      // add name tag to the two input fields
+      // create an array that store all e.currentTarget with the attribute name
+      function handleSubmit(e){
+        e.preventDefault()
+        let formTextFields = [...e.currentTarget.querySelectorAll('[name]')]
+        // console.log(formTextFields)
+
+        let formData = formTextFields.reduce((prev, next) => {
+          let inputName = next.getAttribute('name')
+          let inputValue = next.value
+
+         prev[inputName] = inputValue
+         return prev
+        }, {})
+        addCar(formData.model, formData.regNr)
+        console.log(carSpect)
+      }
+      
+      function handleCallBtn() {
+        callCar()
+      }
+
+function App() {
+
+
   const classes = useStyles()
-
-  function callCar(){
-    let obj = carSpect.shift()
-    console.log(`${obj.model} with registration number ${obj.regNr} come to port 1`)
-    console.log(carSpect)
-    console.log(carSpect2)
-  }
+  
   return (
     <div>
       <CssBaseline />
@@ -62,10 +86,10 @@ function App() {
             <Grid xs={12} sm={12} md={6} item>
             <TextField id="standard-basic" name='model' label="AUDI, BMW ..." />
             <TextField id="standard-basic" name='regNr' label="ABC123 ..." />
-            <Button onClick={callCar} variant="contained" color="primary" fullWidth>
+            <Button onClick={handleCallBtn} variant="contained" color="primary" fullWidth>
                 Call Car
             </Button>
-            <Button type={'submit'}variant="contained" color="primary" fullWidth>
+            <Button type='submit' variant="contained" color="primary" fullWidth>
                 Add
             </Button>
             </Grid>
