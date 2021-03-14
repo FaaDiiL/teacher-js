630ca13f68e63e4dbf09d468f4dae2d3019263a1
NikitaWB <70134216+NikitaWB@users.noreply.github.com>
2021-03-14 12:38:28 +0100

    Started new lesson, created array and functions

--- src/App.js
+++ src/App.js
@@ -14,7 +14,34 @@ const useStyles = makeStyles((theme) => ({
 
 
 function App() {
+  
+    let carSpect = [{model:'AUDI', regNr:'ASD234'}];
 
+  function addCar(model, regNR) {
+    carSpect.push({model, regNR})
+  }
+
+  addCar('Mercedes', 'LOF658')
+  console.log(carSpect)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+ let model = e.target[0].value
+ let regNr = e.target[1].value
+ addCar(model, regNr)
+  }
+   // TODO 1.0
+    // Create a function that remove the first element from an array
+    // The removed element will return values
+    // use the values and create a string like: `(BMW ABC123) come to port 1`
+    // Log the new array
+
+  const handleNext = () => {
+    let obj = carSpect.shift()
+    console.log(`Car ${obj.model} with reg nr ${obj.regNr} is next`)
+    console.log(carSpect)
+  }
+handleNext()
   
   const classes = useStyles()
   return (
@@ -23,11 +50,11 @@ function App() {
       <Container maxWidth="md">
         <h1>CarSpect</h1>
         <Grid container spacing={3}>
-          <form className={classes.root} noValidate autoComplete="off">
+          <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
             <Grid xs={12} sm={12} md={6} item>
             <TextField id="standard-basic" label="AUDI, BMW ..." />
             <TextField id="standard-basic" label="ABC123 ..." />
-              <Button variant="contained" color="primary" fullWidth>
+              <Button type='submit' variant="contained" color="primary" fullWidth>
                 Add
               </Button>
             </Grid>
