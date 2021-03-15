174771a17b5e89773c81e0329b0e0e717c8b8c2d
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-15 23:13:30 +0100

    Finnish base functionality of the project.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Button, Container, CssBaseline, Grid, makeStyles, TextField, Typography } from '@material-ui/core';
+import React, { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,12 +13,28 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const carSpect = [{model:'BMW',regNr:'ABC123'}]
 function App() {
+  const [carSpect, setCarSpect] = useState([{model:'BMW',regNr:'ABC123'},{model:'Toyota',regNr:'ACB145'},{model:'Mazda',regNr:'RTG163'},{model:'VolksWagen',regNr:'IGR834'}])
+  const [callMessage, setCallMessage] = useState('')
 
   function registerCar(model,regNr,port=carSpect){
-    port.push({model,regNr})
+    setCarSpect([...carSpect, {model,regNr}])
   }
+
+  function callCar(e) {
+    if(carSpect.length > 0){
+      let calledCar = carSpect.shift()
+      setCarSpect([...carSpect])
+      setCallMessage(`${calledCar.model} ${calledCar.model} come to port 1`)
+    }else {
+      setCallMessage('No cars to call!')
+      
+      setTimeout(()=>{
+        setCallMessage(null)
+      },3000);
+    }
+  }
+
   function handleSubmit(e) {
     e.preventDefault()
     let model = e.target[0].value
@@ -27,11 +44,7 @@ function App() {
   }
   const classes = useStyles()
 
-  function callCar(){
-    let obj = carSpect.shift()
-    console.log(`${obj.model} with registration number ${obj.regNr} come to port 1`)
-    console.log(carSpect)
-  }
+
   return (
     <div>
       <CssBaseline />
@@ -50,6 +63,14 @@ function App() {
             </Button>
             </Grid>
           </form>
+          <h3>{callMessage}</h3>
+          <ul>
+            {
+              carSpect.map((car)=>(
+                <li key={car.regNr}>{car.model} {car.regNr} </li>
+              ))
+            }
+          </ul>
           </Grid>
       </Container>
     </div>
