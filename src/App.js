637fcf80090c97e2fa7b50702198a1fcc45c007c
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-25 15:52:22 +0100

    Creating the html structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
+
+
+export const MinAndMaxComponent = () => {
+  
+  
+
+  return (
+    <form>
+      <label>Minimum: <input name='minRangeInpEl' /> </label>
+      <label>Maximum: <input name='maxRangeInpEl' /> </label>
+      <button>Add</button>
+    </form>
+  )
+}
+
+export const GuessNumber = () => {
+
+  return (
+    <>
+
+      <label>Minimum: <input name='guessedInpEl' /> </label>
+      <button>Guess</button> 
+    </>
+  )
+}
 
 function App() {
+  
   return (
     <div>
-      
+    <h2>Create a random number!</h2>
+      <MinAndMaxComponent />
+      <p>The number you entered is between: __ and __.</p>
+      <GuessNumber />
     </div>
   )
 }
