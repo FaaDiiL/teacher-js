c19b593b8660ccee0bcea37889e1e51a84a70b1a
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-25 23:57:45 +0100

    Fixing round counter, adding storing array with entered values v1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React,{ useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const MinAndMaxComponent = ({setGeneratedNumber}) => {
-  const [minRangeInpVal, setMinRangeInpVal]=useState('')
-  const [maxRangeInpVal, setMaxRangeInpVal]=useState('')
+export const MinAndMaxComponent = ({ setGeneratedNumber }) => {
+  const [minRangeInpVal, setMinRangeInpVal] = useState('')
+  const [maxRangeInpVal, setMaxRangeInpVal] = useState('')
 
   const CreateRandomNumber = (min, max) => {
     min = Math.ceil(min)
@@ -11,48 +11,54 @@ export const MinAndMaxComponent = ({setGeneratedNumber}) => {
   }
 
   // console.log(` RandomNum: %c${CreateRandomNumber(1,10)}`, "color:green")
-  const handleSubmit = (e)=> {
+  const handleSubmit = (e) => {
     e.preventDefault()
     setGeneratedNumber(CreateRandomNumber(minRangeInpVal, maxRangeInpVal))
   }
 
-  const handleChange = (e)=> {
+  const handleChange = (e) => {
     switch (e.target.name) {
       case 'minRangeInpEl':
         setMinRangeInpVal(e.target.value)
-        break;
+        break
       case 'maxRangeInpEl':
         setMaxRangeInpVal(e.target.value)
-        break;
-    
+        break
+
       default:
-        break;
+        break
     }
-    
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>Minimum: <input onChange={handleChange} name='minRangeInpEl' /> </label>
-      <label>Maximum: <input onChange={handleChange} name='maxRangeInpEl' /> </label>
+      <label>
+        Minimum: <input onChange={handleChange} name='minRangeInpEl' />{' '}
+      </label>
+      <label>
+        Maximum: <input onChange={handleChange} name='maxRangeInpEl' />{' '}
+      </label>
       <button>Add</button>
     </form>
   )
 }
 
-export const GuessNumber = ({setGuessedNum}) => {
+export const GuessNumber = ({ setGuessedNumber }) => {
   const [userGuess, setUserGuess] = useState()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setGuessedNum(userGuess)
+    setGuessedNumber(userGuess)
+    e.target[0].value = ''
   }
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setUserGuess(e.target.value)
   }
   return (
     <form onSubmit={handleSubmit}>
-      <label>Minimum: <input onChange={handleChange} name='guessedInpEl' /> </label>
+      <label>
+        Minimum: <input onChange={handleChange} name='guessedInpEl' />{' '}
+      </label>
       <button>Guess</button>
     </form>
   )
@@ -60,53 +66,53 @@ export const GuessNumber = ({setGuessedNum}) => {
 
 function App() {
   const [generatedNumber, setGeneratedNumber] = useState(undefined)
-  const [guessedNum, setGuessedNum] = useState(undefined)
-  const [guessCounter, setGuessCounter] = useState(undefined)
-  
-  
+  const [guessedNumber, setGuessedNumber] = useState(undefined)
+  const [guessCounter, setGuessCounter] = useState([])
   const [msg, setMsg] = useState('')
 
-  
-  // console.log(` generatedNumber: %c${generatedNumber}`, "color:blue")
-  // console.log(` guessedNum: %c${guessedNum}`, "color:green")
-
-  // Start the 
-  useEffect( () => {
-    
+  // Start the checking when guessedNum is assigned a value
+  useEffect(() => {
     checkGuess()
-      
-  },[guessedNum])
-  
+  }, [guessedNumber])
+
   function checkGuess() {
     // If the random number match the guessed number do this
-    if(generatedNumber == guessedNum){
-      console.log(` Right guess! %c You entered the right number on the ${guessedNum}`, "color:green")
-      setMsg( `You entered the right number on the ${guessedNum}`)
-    } 
-    // else If the random number is larger then the guessed number do this
-    else if(generatedNumber>guessedNum){
-      console.log(`%c ${guessedNum} was to low!`, "color:red")
-    
-      setMsg(`${guessedNum} was to low.`)
+    if (generatedNumber == guessedNumber && guessedNumber != undefined) {
+      console.log(
+        ` Right guess! %c You entered the right number on the ${guessCounter.length} guess!`,
+        'color:green'
+      )
+      setMsg(`You entered the right number on round ${guessCounter.length}!`)
+      setGuessCounter([])
     }
     // else If the random number is larger then the guessed number do this
-    else if(generatedNumber < guessedNum){
-      console.log(`%c ${guessedNum} is to heigh.`, "color:red")
+    else if (generatedNumber > guessedNumber) {
+      setGuessCounter([...guessCounter, guessedNumber])
+      console.log(`%c ${guessedNumber} was to low!`, 'color:red')
 
-      setMsg(`${guessedNum} is to heigh.`)
+      setMsg(`${guessedNumber} was to low.`)
+    }
+    // else If the random number is larger then the guessed number do this
+    else if (generatedNumber < guessedNumber) {
+      setGuessCounter([...guessCounter, guessedNumber])
+
+      console.log(`%c ${guessedNumber} is to heigh.`, 'color:red')
+
+      setMsg(`${guessedNumber} is to heigh.`)
     }
   }
-  
 
   return (
     <div>
-    <h2>Create a random number!</h2>
-      <MinAndMaxComponent setGeneratedNumber={setGeneratedNumber}  />
+      <h2>Create a random number!</h2>
+      <MinAndMaxComponent setGeneratedNumber={setGeneratedNumber} />
       <br />
-      <GuessNumber setGuessedNum={setGuessedNum} guessedNum={guessedNum} />
+      <GuessNumber setGuessedNumber={setGuessedNumber} guessedNumber={guessedNumber} />
+      {
+        msg && <p>{msg} Your guesses: [{guessCounter.join(', ')}]</p>
+      }
     </div>
   )
 }
 
 export default App
-
